1335147429175d2c68cb14613fa132598092980f
Zhe Ren <zherenlee@gmail.com>
2021-08-28 14:38:16 +0800

    Update music.jsx

--- resume-app/src/components/music.jsx
+++ resume-app/src/components/music.jsx
@@ -8,33 +8,37 @@ class Music extends Component {
         <div
           style={{
             backgroundColor: "#F0FFFF",
-            height: "104vh",
+            height: "93vh",
             color: "black",
           }}
         >
+          <br />
           <div className="col-6 offset-3">
             <h1 style={{ textDecoration: "underline" }} className="text-center">
               Experience
             </h1>
-            <div className="offset-1">
+            <br />
+            <div className="text-center">
+              <h5 style={{ fontWeight: "bold" }}>DMAT Graduate of 2017</h5>
               <ul>
-                <h5>DMAT Graduate of 2017</h5>
-                <ul>
-                  <li>Proficient in mixing of audio</li>
-                  <li>Knowledge of multiple instrument miking techniques</li>
-                </ul>
-                <br />
-                <h5>Sound Editor of Some Scuffs Podcast Project</h5>
-                <ul>
-                  <li>
-                    Mixed and provided music for 15 or more podcast episodes
-                  </li>
-                </ul>
-                <br />
-                <h5>Freelancer for Digipen Students</h5>
-                <ul>
-                  <li>Provided music tracks for student projects</li>
-                </ul>
+                <li>Proficient in mixing of audio</li>
+                <li>Knowledge of multiple instrument miking techniques</li>
+              </ul>
+              <br />
+              <h5 style={{ fontWeight: "bold" }}>
+                Sound Editor of Some Scuffs Podcast Project
+              </h5>
+              <ul>
+                <li>
+                  Mixed and provided music for 15 or more podcast episodes
+                </li>
+              </ul>
+              <br />
+              <h5 style={{ fontWeight: "bold" }}>
+                Freelancer for Digipen Students
+              </h5>
+              <ul>
+                <li>Provided music tracks for student projects</li>
               </ul>
             </div>
           </div>
